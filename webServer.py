3a044d142f170978e2e1036a86df956bee57b81e
ah5469 <ah5469@nyu.edu>
2026-02-09 17:06:18 -0500

    Update webServer.py
    Fixed missing headers

--- webServer.py
+++ webServer.py
@@ -37,10 +37,11 @@ def webServer(port=13331):
               
       #Content-Type is an example on how to send a header as bytes. There are more!
       outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
-
+      server_header = b"Server: SimplePythonServer/1.0\r\n"
+      connection_header = b"Connection: close\r\n"
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
-      headers = status + outputdata + b"\r\n"
+      headers = status + outputdata + server_header + connection_header + b"\r\n"
       #Fill in end
                
       file_contents = b""
